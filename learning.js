42cb0f524d7179f46d1fc02a66a82a1f69675f22
FatinChowdhury <122334684+FatinChowdhury@users.noreply.github.com>
2024-05-13 21:45:35 -0400

    Create learning.js

---
+++ learning.js
@@ -0,0 +1,6 @@
+// changes color of text
+document.querySelector("p#weather").style.color = "red";
+// changes margin of area
+document.querySelector("p#weather").style.margin = "100px";
+// changes background color of text (highlighter)
+document.querySelector("p#weather").style.backgroundColor = "white";
