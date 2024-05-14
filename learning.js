670cfa0a63b21e60fc4e10ac70f3ac027c86cba5
FatinChowdhury <122334684+FatinChowdhury@users.noreply.github.com>
2024-05-13 21:50:00 -0400

    Update learning.js

--- learning.js
+++ learning.js
@@ -4,3 +4,7 @@ document.querySelector("p#weather").style.color = "red";
 document.querySelector("p#weather").style.margin = "100px";
 // changes background color of text (highlighter)
 document.querySelector("p#weather").style.backgroundColor = "white";
+
+// refer to change made in CSS class
+document.querySelector("p#weather").classList.add("redbg") 
+// now, this works as highlighter for p#weather text because of changes in CSS
