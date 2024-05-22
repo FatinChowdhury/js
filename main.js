53fe8dcc8fcba5465f31423e8e146d0b01c220be
FatinChowdhury <122334684+FatinChowdhury@users.noreply.github.com>
2024-05-22 16:14:14 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,16 @@ document.querySelector("#open-nav-menu").addEventListener("click", function(){
 document.querySelector("#close-nav-menu").addEventListener("click", function(){
     document.querySelector("header nav .wrapper").classList.remove("nav-open");
 });
+
+// Greeting Section
+
+const greetingText = "good morning"
+const weatherCondition = "sunny"
+const userLocation = "new york"
+let temperature = 22.24243
+let weatherText = `The weather is ${weatherCondition} in ${userLocation}
+and it's ${temperature.toString()}°C outside.`
+
+
+document.querySelector("#greeting").innerHTML = greetingText
+document.querySelector("p#weather").innerHTML = weatherText
