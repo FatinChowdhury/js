ed182c2c8beb6e4ad4779c85c430e66f392eac2c
FatinChowdhury <122334684+FatinChowdhury@users.noreply.github.com>
2024-05-23 01:43:28 -0400

    Update main.js
    made handler functions for each sections, placed const at top for legibility

--- main.js
+++ main.js
@@ -1,57 +1,3 @@
-document.querySelector("#open-nav-menu").addEventListener("click", function(){
-    document.querySelector("header nav .wrapper").classList.add("nav-open");
-});
-
-document.querySelector("#close-nav-menu").addEventListener("click", function(){
-    document.querySelector("header nav .wrapper").classList.remove("nav-open");
-});
-
-
-const greetingText = "good morning"
-const weatherCondition = "sunny"
-const userLocation = "new york"
-let temperature = 22
-
-
-let celsiusText = `The weather is ${weatherCondition} in ${userLocation}
-and it's ${temperature.toString()}°C outside.`
-
-let fahrText = `The weather is ${celsiusToFahr(temperature)}°C in ${userLocation}`
-
-
-function celsiusToFahr(temperature) {
-    return temperature * 9/5 + 32
-}
-
-document.querySelector("#greeting").innerHTML = greetingText
-document.querySelector("p#weather").innerHTML = celsiusText
-
-
-document.querySelector(".weather-group").addEventListener("click", function(e) {
-    // celsius
-    // fahr
-
-    if (e.target.id == "celsius") {
-        document.querySelector("p#weather").innerHTML = celsiusText
-    } else if (e.target.id == "fahr") {
-        document.querySelector("p#weather").innerHTML = fahrText
-    }
-});
-
-// new Date().getHours(), getMinutes(), getSeconds()
-
-// 
-setInterval(function(){
-    let localTime = new Date()
-    document.querySelector("span[data-time=hours]").textContent = localTime.getHours().toString().padStart(2, "0")
-    // having 09 and not 9 for time.
-    document.querySelector("span[data-time=minutes]").textContent = localTime.getMinutes().toString().padStart(2, "0")
-    document.querySelector("span[data-time=seconds]").textContent = localTime.getSeconds().toString().padStart(2, "0")
-}, 1000)
-
-// Gallery Section
-// src="./assets/gallery/image1.jpg" alt="Thumbnail Image 1"
-
 const galleryImages = [
     {
         src: "./assets/gallery/image1.jpg",
@@ -69,23 +15,144 @@ const galleryImages = [
     },
 ]
 
-let mainImage = document.querySelector("#gallery > img")
-let thumbnails = document.querySelector("#gallery .thumbnails")
 
-mainImage.src = galleryImages[0].src
-mainImage.alt = galleryImages[0].alt
+// Menu Section
 
-// <img src="./assets/gallery/image1.jpg"
-// alt="Thumbnail Image 1" 
-// data-array-index="0" data-selected="true">
+function menuHandler() {
 
-// adding code into the html section (removed thumbnails in html)
-galleryImages.forEach(function(image, index){
-    let thumb = document.createElement("img")
-    thumb.src = image.src
-    thumb.alt = image.alt
-    thumb.dataset.arrayIndex = index
-    thumb.dataset.selected = false
+    document.querySelector("#open-nav-menu").addEventListener("click", function(){
+        document.querySelector("header nav .wrapper").classList.add("nav-open");
+    });
+    
+    document.querySelector("#close-nav-menu").addEventListener("click", function(){
+        document.querySelector("header nav .wrapper").classList.remove("nav-open");
+    });
+    
+}
 
-    thumbnails.appendChild(thumb)
-})
+// Temperature Conversion
+function celsiusToFahr(temperature) {
+    return temperature * 9/5 + 32
+}
+// Greeting Section
+
+function greetingHandler() {
+
+    let currentHour = new Date().getHours()
+    let greetingText
+    
+    if (currentHour < 12) {
+        greetingText = "Good Morning!"
+    }
+    else if (currentHour < 19) {
+        greetingText = "Good Afternoon!"
+    }
+    else if (currentHour < 24) {
+        greetingText = "Good Evening!"
+    
+    } else {
+        greetingText = "Welcome!"
+    }
+    
+    const weatherCondition = "sunny"
+    const userLocation = "new york"
+    let temperature = 22
+    
+    
+    let celsiusText = `The weather is ${weatherCondition} in ${userLocation}
+    and it's ${temperature.toString()}°C outside.`
+    
+    let fahrText = `The weather is ${celsiusToFahr(temperature)}°C in ${userLocation}`
+    
+    
+    
+    document.querySelector("#greeting").innerHTML = greetingText
+    document.querySelector("p#weather").innerHTML = celsiusText
+    
+    
+    document.querySelector(".weather-group").addEventListener("click", function(e) {
+        // celsius
+        // fahr
+    
+        if (e.target.id == "celsius") {
+            document.querySelector("p#weather").innerHTML = celsiusText
+        } else if (e.target.id == "fahr") {
+            document.querySelector("p#weather").innerHTML = fahrText
+        }
+    });
+}
+
+
+// new Date().getHours(), getMinutes(), getSeconds()
+// Local Time Selection
+function clockHandler() {
+
+    setInterval(function(){
+        let localTime = new Date()
+        document.querySelector("span[data-time=hours]").textContent = localTime.getHours().toString().padStart(2, "0")
+        // having 09 and not 9 for time.
+        document.querySelector("span[data-time=minutes]").textContent = localTime.getMinutes().toString().padStart(2, "0")
+        document.querySelector("span[data-time=seconds]").textContent = localTime.getSeconds().toString().padStart(2, "0")
+    }, 1000)
+}
+
+
+// Gallery Section
+// src="./assets/gallery/image1.jpg" alt="Thumbnail Image 1"
+
+function galleryHandler() {
+
+    let mainImage = document.querySelector("#gallery > img")
+    let thumbnails = document.querySelector("#gallery .thumbnails")
+    
+    mainImage.src = galleryImages[0].src
+    mainImage.alt = galleryImages[0].alt
+    
+    // <img src="./assets/gallery/image1.jpg"
+    // alt="Thumbnail Image 1" 
+    // data-array-index="0" data-selected="true">
+    
+    // adding code into the html section (removed thumbnails in html)
+    galleryImages.forEach(function(image, index){
+        let thumb = document.createElement("img")
+        thumb.src = image.src
+        thumb.alt = image.alt
+        thumb.dataset.arrayIndex = index
+        thumb.dataset.selected = index === 0 ? true : false;    // if index is 0, then true, else false
+    
+        // to select different images s.t. it changes pics
+        thumb.addEventListener("click", function(e){
+            let selectedIndex = e.target.dataset.arrayIndex
+            let selectedImage = galleryImages[selectedIndex]
+    
+            mainImage.src = selectedImage.src
+            mainImage.alt = selectedImage.alt
+            
+            // unselects (white highlight) the images when diff img clicked
+            thumbnails.querySelectorAll("img").forEach(function(img) {
+                img.dataset.selected = false
+            })
+            // selects when diff img clicked (white marker shows for diff img clicked on)
+            e.target.dataset.selected = true
+    
+        })
+    
+        // if (index === 0) {
+        //     thumb.dataset.selected = true;
+        // }
+        // else {
+        //     thumb.dataset.selected = false;
+        // }
+    
+    
+    
+    
+        thumbnails.appendChild(thumb)
+    })
+
+}
+
+// Page Load
+menuHandler();
+greetingHandler();
+clockHandler();
